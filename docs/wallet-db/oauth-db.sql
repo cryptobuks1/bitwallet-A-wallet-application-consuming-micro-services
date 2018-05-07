ca2658db48aa6d7e626569cc95428925000f6daa
Afshar <afshartech@gmail.com>
2018-05-07 13:53:15 +0200

    Updated oauth-db sql script with table creation scripts

--- docs/wallet-db/oauth-db.sql
+++ docs/wallet-db/oauth-db.sql
@@ -10,3 +10,34 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`oauth` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `oauth`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `enabled` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` VARCHAR(50) NOT NULL,
+  `authority` VARCHAR(50) NOT NULL,
+  UNIQUE KEY `ix_auth_username` (`username`,`authority`)
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `oauth_client_details`;
+CREATE TABLE `oauth_client_details` (
+  `client_id` VARCHAR(256) NOT NULL,
+  `resource_ids` VARCHAR(256) DEFAULT NULL,
+  `client_secret` VARCHAR(256) DEFAULT NULL,
+  `scope` VARCHAR(256) DEFAULT NULL,
+  `authorized_grant_types` VARCHAR(256) DEFAULT NULL,
+  `web_server_redirect_uri` VARCHAR(256) DEFAULT NULL,
+  `authorities` VARCHAR(256) DEFAULT NULL,
+  `access_token_validity` INT(11) DEFAULT NULL,
+  `refresh_token_validity` INT(11) DEFAULT NULL,
+  `additional_information` VARCHAR(4096) DEFAULT NULL,
+  `autoapprove` VARCHAR(256) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
